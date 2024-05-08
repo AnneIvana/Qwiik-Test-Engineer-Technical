b956bd09013bd1d2ac8576843f3b6c3ec81206bf
Anne <anne@dagangan.com>
2024-05-08 15:13:43 +0700

    main

---
+++ Logout.js
@@ -0,0 +1,7 @@
+</thead>
+<tbody>
+<tr><td>open</td><td>https://www.saucedemo.com/inventory.html<datalist><option>https://www.saucedemo.com/inventory.html</option></datalist></td><td></td></tr>
+<tr><td>click</td><td>id=logout_sidebar_link<datalist><option>id=logout_sidebar_link</option><option>link=Logout</option><option>xpath=//a[contains(text(),'Logout')]</option><option>xpath=//a[@id='logout_sidebar_link']</option><option>xpath=//div[@id='menu_button_container']/div/div[2]/div/nav/a[3]</option><option>xpath=(.//*[normalize-space(text()) and normalize-space(.)='About'])[1]/following::a[1]</option><option>xpath=(.//*[normalize-space(text()) and normalize-space(.)='Open Menu'])[1]/following::a[3]</option><option>xpath=(.//*[normalize-space(text()) and normalize-space(.)='Close Menu'])[1]/preceding::a[2]</option><option>xpath=(.//*[normalize-space(text()) and normalize-space(.)='Swag Labs'])[2]/preceding::a[2]</option><option>xpath=//*/text()[normalize-space(.)='Logout']/parent::*</option><option>xpath=//a[3]</option><option>css=#logout_sidebar_link</option></datalist></td><td></td></tr>
+</tbody></table>
+</body>
+</html>
---
+++ Qwiik Test Engineer Technical/Login.js
@@ -0,0 +1,11 @@
+</thead>
+<tbody>
+<tr><td>open</td><td>https://www.saucedemo.com/<datalist><option>https://www.saucedemo.com/</option></datalist></td><td></td></tr>
+<tr><td>click</td><td>id=user-name<datalist><option>id=user-name</option><option>name=user-name</option><option>xpath=//input[@id='user-name']</option><option>xpath=//div[@id='login_button_container']/div/form/div/input</option><option>xpath=//input</option><option>css=#user-name</option></datalist></td><td></td></tr>
+<tr><td>type</td><td>id=user-name<datalist><option>id=user-name</option><option>name=user-name</option><option>xpath=//input[@id='user-name']</option><option>xpath=//div[@id='login_button_container']/div/form/div/input</option><option>xpath=//input</option><option>css=#user-name</option></datalist></td><td>standard_user</td></tr>
+<tr><td>click</td><td>id=password<datalist><option>id=password</option><option>name=password</option><option>xpath=//input[@id='password']</option><option>xpath=//div[@id='login_button_container']/div/form/div[2]/input</option><option>xpath=//div[2]/input</option><option>css=#password</option></datalist></td><td></td></tr>
+<tr><td>type</td><td>id=password<datalist><option>id=password</option><option>name=password</option><option>xpath=//input[@id='password']</option><option>xpath=//div[@id='login_button_container']/div/form/div[2]/input</option><option>xpath=//div[2]/input</option><option>css=#password</option></datalist></td><td>secret_sauce</td></tr>
+<tr><td>click</td><td>id=login-button<datalist><option>id=login-button</option><option>name=login-button</option><option>xpath=//input[@id='login-button']</option><option>xpath=//div[@id='login_button_container']/div/form/input</option><option>xpath=//form/input</option><option>css=#login-button</option></datalist></td><td></td></tr>
+</tbody></table>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
